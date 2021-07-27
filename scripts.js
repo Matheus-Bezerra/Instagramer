2aae8eb818c3b3566e3d88cdb25a8a80fa7b85b2
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-07-27 15:12:20 -0300

    Adicionando Modo Claro/modo noturno na página

--- scripts.js
+++ scripts.js
@@ -1,6 +1,20 @@
 const modoNoturno = document.querySelector('.conteudoModoNoturno')
+const body = document.querySelector('body')
+const conteudo = document.querySelector('.conteudos')
+const cabecalho = document.querySelector('.cabecalho')
+const container = document.querySelector('.container')
+const links = document.querySelectorAll('a')
+console.log(conteudo)
+
 console.log(modoNoturno)
 modoNoturno.addEventListener('click', handleClick)
 function handleClick(event) {
-    console.log('Opa')
+    modoNoturno.classList.toggle('ativo')
+    conteudo.classList.toggle('ativo')
+    body.classList.toggle('ativo')
+    cabecalho.classList.toggle('ativo')
+    container.classList.toggle('ativo')
+    links.forEach((link) => {
+        link.classList.toggle('ativo')
+    })
 }
