ad38a4099dbdc76ccd9942de637d761a9a5fa950
Matheus-Bezerra <matheusbwzerra002@gmail.com>
2021-07-27 14:37:18 -0300

    Adicioando função para modo noturno na página

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const modoNoturno = document.querySelector('.conteudoModoNoturno')
+console.log(modoNoturno)
+modoNoturno.addEventListener('click', handleClick)
+function handleClick(event) {
+    console.log('Opa')
+}
